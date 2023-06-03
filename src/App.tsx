49b472067922ccc51b210daca7609405b02ed4e3
Jules Montojo <66418803+Jaymontojo@users.noreply.github.com>
2023-06-03 16:28:49 +0900

    feat: implement styled navbar (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import LineChart from "./components/LineChart";
+import Navbar from "./components/Navbar";
 import { Devices } from "./components/Devices";
 import "./styles/App.css";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <LineChart />
       <Devices />
     </div>
