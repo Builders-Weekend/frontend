06c4696d145412b16e48a3fbb906ebfdb20394c8
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-06-03 20:45:15 +0900

    Style: Finish basic styling for pull out component

--- src/components/AddDeviceForm.tsx
+++ src/components/AddDeviceForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import "../styles/AddDeviceForm.css";
 
 function AddDeviceForm() {
   const [formData, setFormData] = useState({
@@ -30,31 +31,29 @@ function AddDeviceForm() {
 
   return (
     <div className="container">
-      <div className="modal-container">
-        <div className="modal-content">
-          <h1>Add Device</h1>
-          <form
-            onSubmit={handleSubmit}
-            id="add-device-form"
-            className="modal-form"
-          >
-            <label htmlFor="input-name">Name</label>
+      <div className="form-container">
+        <div className="form-content">
+          <h1>Add a New Device</h1>
+          <form onSubmit={handleSubmit} id="add-device-form" className="form">
+            <label htmlFor="input-name">Device Name</label>
             <input
               required
               type="text"
               inputMode="text"
               id="input-name"
               value={formData.name}
+              className="text-input"
               onChange={(e) =>
                 setFormData({ ...formData, name: e.target.value })
               }
             />
 
-            <label htmlFor="input-consumption">Consumption</label>
+            <label htmlFor="input-consumption">Consumption (kw/h)</label>
             <input
               required
               type="number"
               inputMode="numeric"
+              className="number-input"
               id="input-consumption"
               value={formData.consumption}
               onChange={(e) =>
@@ -65,21 +64,24 @@ function AddDeviceForm() {
               }
             />
 
-            <label htmlFor="input-battery">Battery</label>
-            <input
-              type="checkbox"
-              id="input-battery"
-              checked={formData.isBattery}
-              onChange={(e) =>
-                setFormData({ ...formData, isBattery: e.target.checked })
-              }
-            />
+            <div className="checkbox-container">
+              <input
+                type="checkbox"
+                id="input-battery"
+                checked={formData.isBattery}
+                onChange={(e) =>
+                  setFormData({ ...formData, isBattery: e.target.checked })
+                }
+              />
+              <label htmlFor="input-battery">Battery</label>
+            </div>
 
-            <label htmlFor="input-charge-level">Charge Level</label>
+            <label htmlFor="input-charge-level">Charge Level (%)</label>
             <input
               required
               type="number"
               inputMode="numeric"
+              className="number-input"
               id="input-charge-level"
               value={formData.chargeLevel}
               onChange={(e) =>
@@ -90,7 +92,7 @@ function AddDeviceForm() {
               }
             />
 
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Submit" className="submit-button" />
           </form>
         </div>
       </div>
