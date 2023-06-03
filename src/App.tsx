912d4f84afc158ce29690ff33ccccb33533d49d4
Chad Grover <93070367+chadgrover@users.noreply.github.com>
2023-06-03 17:08:33 +0900

    PR: Implement AddDeviceModal component (#10)
    * Feat: Touch PopupModal.tsx
    
    * Style: Implement PopupModal and basic CSS styling
    
    * Feat: Touch DeviceForm component
    
    * Feat: Add form functionality for AddDeviceModal
    
    * Refactor: Add success message and reset form functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import LineChart from "./components/LineChart";
 import Navbar from "./components/Navbar";
 import { Devices } from "./components/Devices";
+import AddDeviceModal from "./components/AddDeviceModal";
 import "./styles/App.css";
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState<boolean>(false);
+
+  function handleModalOpen() {
+    setModalIsVisible(true);
+  }
+
   return (
-    <div>
+    <div className="app-container">
       <Navbar />
       <LineChart />
       <Devices />
+      <AddDeviceModal
+        modalIsVisible={modalIsVisible}
+        setModalIsVisible={setModalIsVisible}
+      />
+      <button onClick={handleModalOpen}>Add Device</button>
     </div>
   );
 }
