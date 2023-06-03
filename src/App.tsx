a0223f8e304a69e4e2be9cdd2ae9d9b65f426080
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-06-03 15:25:51 +0900

    Style: Implement PopupModal and basic CSS styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import LineChart from "./components/LineChart";
+import PopupModal from "./components/PopupModal";
 import "./styles/App.css";
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState<boolean>(false);
+
+  function handleModalOpen() {
+    setModalIsVisible(true);
+  }
+
   return (
-    <div>
+    <div className="app-container">
       <LineChart />
+      <PopupModal
+        modalIsVisible={modalIsVisible}
+        setModalIsVisible={setModalIsVisible}
+      />
+      <button onClick={handleModalOpen}>Open Modal</button>
     </div>
   );
 }
--- src/components/PopupModal.tsx
+++ src/components/PopupModal.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import "../styles/PopupModal.css";
 
-function PopupModal() {
-    return <h1>Modal</h1>
+interface IPopupModalProps {
+  modalIsVisible: boolean;
+  setModalIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function PopupModal({ modalIsVisible, setModalIsVisible }: IPopupModalProps) {
+  const handleModalClose = () => {
+    setModalIsVisible(false);
+  };
+
+  return (
+    <>
+      {modalIsVisible && (
+        <div className="container">
+          <div className="modal-container">
+            <div className="modal-content">
+              <p>Modal text goes here</p>
+              <button onClick={handleModalClose}>Close Modal</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default PopupModal;
