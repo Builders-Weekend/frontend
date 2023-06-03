43823a382ac0a9aec4a232e373680d77f084221f
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-06-03 13:49:14 +0900

    Fix: Correct typing issues in LineChart

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,10 @@
 import LineChart from "./components/LineChart";
-
-const labels = ["January", "February", "March", "April", "May", "June, July"];
-
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: [0, 10, 5, 2, 20, 30],
-      fill: false,
-      borderColor: "rgb(75, 192, 192)",
-      tension: 0.1,
-    },
-  ],
-};
-
-const options = {
-  responsive: true,
-  title: {
-    display: true,
-    text: "My Line Chart",
-  },
-  scales: {
-    y: {
-      beginAtZero: true,
-      title: {
-        display: true,
-        text: "Value",
-      },
-    },
-  },
-};
+import "./styles/App.css";
 
 function App() {
   return (
-    <div className="container">
-      <LineChart data={data} options={options}/>
+    <div>
+      <LineChart />
     </div>
   );
 }
--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,16 +1,63 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
-import { ChartData } from "chart.js";
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale, //x-axis
+  LinearScale, //y-axis
+  PointElement,
+  Legend,
+  Tooltip,
+} from "chart.js";
+import { ChartData, ChartOptions } from "chart.js";
 import "../styles/LineChart.css";
 
-interface ILineChartProps {
-  data: ChartData<"line", number[], unknown>;
-  options?: any;
-}
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Legend,
+  Tooltip
+);
 
-const LineChart: React.FC<ILineChartProps> = ({ data, options }) => {
+const data: ChartData<"line", number[], unknown> = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [0, 10, 5, 2, 20, 30],
+      backgroundColor: "aqua",
+      borderColor: "black",
+      pointBorderColor: "black",
+      fill: true,
+      tension: 0.4,
+    },
+  ],
+};
+
+const options: ChartOptions<'line'> = {
+  responsive: true,
+  scales: {
+    x: {
+      type: 'category',
+    },
+    y: {
+      beginAtZero: true,
+      title: {
+        display: true,
+        text: "Value"
+      },
+      ticks: {
+        stepSize: 1,
+      },
+    },
+  },
+};
+
+const LineChart = () => {
   return (
-    <div className="chart-container">
+    <div className="line-chart-container">
       <Line data={data} options={options} />
     </div>
   );
