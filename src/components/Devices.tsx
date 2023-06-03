90f999799598146d418cc32de94c91cfb728eca4
Dominic Lowes <chaiskeng@gmail.com>
2023-06-03 15:53:19 +0900

    feat: render device details upon successful API call

--- src/components/Devices.tsx
+++ src/components/Devices.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Device } from "../utils/types";
 
 export const Devices = () => {
+    const [devices, setDevices] = useState<Device[]>([]);
+
+    const fetchDeviceData = async () => {
+        try {
+            const response = await axios.get("http://localhost:4000/api/devices");
+            setDevices(response.data);
+            console.log(response.data)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchDeviceData();
+    }, []);
+
     return (
         <div>
             <h1>Devices</h1>
+            {devices.map((device) => (
+                <div key={device.name}>
+                    <h2>{device.name}</h2>
+                    <p>Consumption: {device.consumption}</p>
+                </div>
+            ))}    
         </div>
     );
 };
