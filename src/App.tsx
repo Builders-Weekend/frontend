e2507c77840ed9024e0fb9408f6c260c91c6b8a9
Dominic Lowes <99005923+SodaJyu@users.noreply.github.com>
2023-06-03 15:59:06 +0900

    7 there should be a devices modal (#8)
    * feat: create Devices component
    
    * chore: add Devices component to App
    
    * feat: create custom types for external API data
    
    * feat: render device details upon successful API call
    
    * chore: add classname to mapped devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import LineChart from "./components/LineChart";
+import { Devices } from "./components/Devices";
 import "./styles/App.css";
 
 function App() {
   return (
     <div>
       <LineChart />
+      <Devices />
     </div>
   );
 }
---
+++ src/utils/types.ts
@@ -0,0 +1,32 @@
+type Device = {
+    name: string;
+    consumption: number;
+    isBattery?: boolean;
+    chargeLevel?: number;
+}
+
+type weatherApiResponse = {
+    forecast: Forecast
+}
+type Forecast = {
+    forecastday: ForecastDay[]
+}
+
+type ForecastDay = {
+    date: string,
+    hour: Hour[]
+}
+
+type Hour = {
+    time: string,
+    windKph: number,
+    uvIndex: number
+}
+
+type PricingData = {
+    price: number,
+    valid_from: string,
+    valid_to: string
+}
+
+export type { Device, weatherApiResponse, Forecast, ForecastDay, Hour, PricingData };
