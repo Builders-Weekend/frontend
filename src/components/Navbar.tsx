205639112804e2b2d3d1969afa1c92b5e2b7c16b
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-06-04 15:05:04 +0900

    Style: Add basic logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import '../styles/Navbar.css';
+import React from "react";
+import "../styles/Navbar.css";
+import "../styles/fonts.css";
 
 export default function Navbar() {
   return (
-    <div className='navbar'>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <span className="navbar-logo-text">Croc'n</span>
+      </div>
+    </nav>
+  );
 }
